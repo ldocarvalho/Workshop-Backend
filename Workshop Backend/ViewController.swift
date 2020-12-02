1d3f576ea556cf264aa693efdcf1449f74704309
Rodrigo Lemos <rodlemoss@gmail.com>
2020-12-02 14:52:12 -0400

    primeira tela com conexoes de iboutlet e ibactions

--- Workshop Backend/ViewController.swift
+++ Workshop Backend/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
+    @IBOutlet weak var progresssBar: UIProgressView!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
 }
 
